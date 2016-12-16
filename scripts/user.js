e2e43e1f0096c8567b31c05f1895f4ae47ae29b7
Roberto Ortega <berto.ort@gmail.com>
2016-12-16 12:06:59 -0700

    :sparkles: Add user page

---
+++ scripts/user.js
@@ -0,0 +1,54 @@
+const API_URL = 'http://localhost:3000';
+
+$(document).ready(function () {
+  // get user id from url query
+  const params = parseQuery(window.location.search);
+  // make a request to the server for the user information
+  getUserInfo(params.id)
+    .then(addUserInfoToPage)
+    .then(getStickers)
+    .then(addStickers)
+    .catch(weSuck);
+  // show user information
+  // make a request to server for the stickers for the user with that id
+  // show user stickers
+});
+
+function parseQuery(query) {
+  return query.substr(1).split('&').reduce((params, keyValue) => {
+    const parts = keyValue.split('=');
+    params[parts[0]] = parts[1];
+    return params
+  }, {});
+}
+
+function getUserInfo(id) {
+  return $.get(`${API_URL}/user/${id}`)
+}
+
+function getStickers(id) {
+  return $.get(`${API_URL}/user/${id}/sticker`)
+}
+
+function addUserInfoToPage(user) {
+  let source = $("#user-template").html();
+  let template = Handlebars.compile(source);
+  let context = user;
+  let html = template(context);
+  $('.user').html(html);
+  return user.id;
+}
+
+function addStickers(stickers) {
+  let source = $("#sticker-template").html();
+  let template = Handlebars.compile(source);
+  let context = {stickers};
+  // yay more variations!! abstraction rocks!
+  // let context = {stickers: stickers};
+  let html = template(context);
+  $('.stickers').html(html);
+}
+
+function weSuck() {
+  alert('user not found... and we suck')
+}
